714db4fdac42049506f0e5ca500e803b5874d9d0
AnnBalias <annbalias@gmail.com>
2025-09-16 12:52:03 +0300

    project-init

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+import compression from 'vite-plugin-compression';
+import Inspect from 'vite-plugin-inspect';
+import { visualizer } from 'rollup-plugin-visualizer';
+import type { PluginOption } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      typescript: true,
+    }),
+    compression({
+      algorithm: 'gzip',
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+    }),
+    Inspect(),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          open: false,
+          filename: 'dist/stats.html',
+        }) as PluginOption,
+      ],
+    },
+  },
+});
